4f4c640ec2be6cd59f37429d2f37f37d77dd8f8a
zhb <308109106@qq.com>
2019-09-26 08:09:54 +0000

    Add template index

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask import url_for
 app = Flask(__name__)
+
+name = 'Mr Zhuang'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
-def hello():
-    return 'Welcome to My Watchlist!'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -14,7 +28,7 @@ def user_page(name):
 
 @app.route('/test')
 def test_url_for():
-    print(url_for('hello'))
+    print(url_for('index'))
     print(url_for('user_page',name='zhuanghaibin'))
     print(url_for('test_url_for'))
     print(url_for('test_url_for',num=2))
